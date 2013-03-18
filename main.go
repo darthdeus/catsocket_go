9091638ee07fd896722cf0677647487708c2403d
Jakub Arnold <darthdeus@gmail.com>
2013-03-18 13:06:04 +0100

    Add datasource polling

--- main.go
+++ main.go
@@ -9,17 +9,23 @@ import (
 	"time"
 )
 
+func pollDataSource(w http.ResponseWriter, c redis.Conn) {
+	reply, _ := redis.Values(c.Do("ZRANGEBYSCORE", "foo", 0, int(1e9)))
+
+	for _, item := range reply {
+		// text := string(item.([]byte))
+		text, _ := redis.String(item, nil)
+		fmt.Printf("%s", text)
+		io.WriteString(w, string(text))
+	}
+}
+
 func CreateGetHandler(in chan string, c redis.Conn) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		reply, _ := redis.Values(c.Do("ZRANGEBYSCORE", "foo", 0, 1e6))
 
-		<-in
-
-		for _, item := range reply {
-			text := string(item.([]byte))
-			// text, _ := redis.String(item, nil)
-			fmt.Printf("%s", text)
-			io.WriteString(w, string(text))
+		for i := 0; i < 5; i += 1 {
+			pollDataSource(w, c)
+			time.Sleep(500 * time.Millisecond)
 		}
 	}
 }
