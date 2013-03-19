0d8648aea5d7e751460850a08c9dc0e3fb158102
Jakub Arnold <darthdeus@gmail.com>
2013-03-19 01:58:51 +0100

    Cleanup stuff

--- db.go
+++ db.go
@@ -5,21 +5,22 @@ import (
 	"time"
 )
 
+// TODO - try using a mutex to force atomic operations
 type DB struct {
-	pool *redis.Pool
+  pool *redis.Pool
 }
 
 func (d DB) Get() DBConnection {
-	return DBConnection{d.pool.Get()}
+  return DBConnection{d.pool.Get()}
 }
 
 type Connection interface {
-	AuthorizeKey(apiKey string) bool
-	Poll(channel string, score int) ([]interface{}, error)
+  AuthorizeKey(apiKey string) bool
+  Poll(channel string, score int) ([]interface{}, error)
 }
 
 type DBConnection struct {
-	redis.Conn
+  redis.Conn
 }
 
 func (c DBConnection) AuthorizeKey(apiKey string) bool {
@@ -35,31 +36,33 @@ func (c DBConnection) AuthorizeKey(apiKey string) bool {
 func (c DBConnection) Poll(channel string, score int) ([]interface{}, error) {
 	reply, err := redis.Values(c.Do("ZRANGEBYSCORE", channel, 0, score))
 
-	return reply, err
+  return reply, err
 }
 
 func ChannelName(apiKey string, name string) string {
 	return apiKey + name
 }
 
+
 func CreateConnection() DB {
-	server := "0.0.0.0:6379"
+  server := "0.0.0.0:6379"
 
-	pool := &redis.Pool{
-		MaxIdle:     3,
-		IdleTimeout: 240 * time.Second,
-		Dial: func() (redis.Conn, error) {
-			c, err := redis.Dial("tcp", server)
-			if err != nil {
-				return nil, err
-			}
-			return c, err
-		},
-		TestOnBorrow: func(c redis.Conn, t time.Time) error {
-			_, err := c.Do("PING")
-			return err
-		},
-	}
+  pool := &redis.Pool{
+    MaxIdle: 3,
+    IdleTimeout: 240 * time.Second,
+    Dial: func () (redis.Conn, error) {
+      c, err := redis.Dial("tcp", server)
+      if err != nil {
+        return nil, err
+      }
+      return c, err
+    },
+    TestOnBorrow: func(c redis.Conn, t time.Time) error {
+      _, err := c.Do("PING")
+      return err
+    },
+  }
 
-	return DB{pool}
+  return DB{pool}
 }
+
--- util.go
+++ util.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/garyburd/redigo/redis"
 	"io"
-	"log"
+  "log"
 	"net/http"
 	_ "net/http/pprof"
 )
@@ -16,15 +16,20 @@ func check(err error) {
 }
 
 func pollDataSource(w http.ResponseWriter, c DBConnection) {
-	reply, err := c.Poll("foo", int(1e9))
+  reply, err := c.Poll("foo", int(1e9))
 
-	check(err)
+  // TODO - this should actually just return a channel and the connection
+  // should block on it until there is some data. That way we can easily
+  // multiplex connections onto a few channels and re-broadcast them.
+  // Maybe even return a struct containing the response and the channel?
 
-	if len(reply) > 0 {
-		fmt.Fprintf(w, "Got a reply\n")
-	} else {
-		fmt.Fprintf(w, "Empty reply\n")
-	}
+  check(err)
+
+  if len(reply) > 0 {
+    fmt.Fprintf(w, "Got a reply\n")
+  } else {
+    fmt.Fprintf(w, "Empty reply\n")
+  }
 
 	for _, item := range reply {
 		text, _ := redis.String(item, nil)
@@ -39,29 +44,29 @@ func httpError(w http.ResponseWriter, status int, text string) {
 }
 
 type Item struct {
-	Valid   bool
-	Data    string
-	ApiKey  string
-	Channel string
+  Valid bool
+  Data string
+  ApiKey string
+  Channel string
 }
 
 func ParseRequest(w http.ResponseWriter, req *http.Request, c Connection) Item {
-	data := req.FormValue("data")
-	apiKey := req.FormValue("api_key")
-	channel := req.FormValue("channel")
+  data := req.FormValue("data")
+  apiKey := req.FormValue("api_key")
+  channel := req.FormValue("channel")
 
-	invalid := Item{}
-	invalid.Valid = false
+  invalid := Item{}
+  invalid.Valid = false
 
-	if apiKey == "" || data == "" || channel == "" {
-		httpError(w, 400, "channel, api_key and data are all required")
-		return invalid
-	}
+  if apiKey == "" || data == "" || channel == "" {
+    httpError(w, 400, "channel, api_key and data are all required")
+    return invalid
+  }
 
-	if c.AuthorizeKey(apiKey) {
-		return Item{true, data, apiKey, channel}
-	}
+  if c.AuthorizeKey(apiKey) {
+    return Item{true, data, apiKey, channel}
+  }
 
-	httpError(w, 401, "authentication failed")
-	return invalid
+  httpError(w, 401, "authentication failed")
+  return invalid
 }
